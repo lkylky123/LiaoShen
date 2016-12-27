681673232ede49a3ea850e730071516654958434
lkylky123 <lkylky123>
2016-12-27 16:57:12 +0800

    修改功能

--- myapplicationtext/src/main/java/com/example/myapplicationtext/MainActivity.java
+++ myapplicationtext/src/main/java/com/example/myapplicationtext/MainActivity.java
@@ -18,6 +18,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
 
 
         try {
